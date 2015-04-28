838cd8ff004de51ce03427eafaa994b38274bfe7
vmedvedovskiy <vmedvedovsky@infostroy.com.ua>
2015-04-28 14:23:04 +0300

    front-end part

---
+++ CoreApi.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CoreApi.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoreApi.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed0d603e-aae2-4f03-801d-0a73c96611fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CoreApi.Web/main.js
@@ -0,0 +1,67 @@
+﻿(function () {
+    var data = null;
+    var graph = null;
+
+    google.load("visualization", "1");
+
+    // Set callback to run when API is loaded
+    google.setOnLoadCallback(drawVisualization);
+
+    // Called when the Visualization API is loaded.
+    function drawVisualization() {
+        // Create and populate a data table.
+        data = new google.visualization.DataTable();
+        data.addColumn('number', 'x');
+        data.addColumn('number', 'y');
+        data.addColumn('number', 'value');
+
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:9000/api/sphere/points',
+            data: JSON.stringify({
+                step: 0.05,
+                xBounds: {
+                    low: -2,
+                    high: 2
+                },
+                yBounds: {
+                    low: -1,
+                    high: 1
+                }
+            }),
+            success: function (result, status, xhr) {
+                var pointsLength = result.length;
+                for (var i = 0; i < pointsLength; ++i) {
+                    data.addRow([result[i].X, result[i].Y, result[i].Z]);
+                }
+
+                // create some nice looking data with sin/cos
+                var steps = 25;  // number of datapoints will be steps*steps
+                var axisMax = 314;
+                axisStep = axisMax / steps;
+
+
+                // specify options
+                options = {
+                    width: "400px",
+                    height: "400px",
+                    style: "surface",
+                    showPerspective: true,
+                    showGrid: true,
+                    showShadow: false,
+                    keepAspectRatio: true,
+                    verticalRatio: 0.5
+                };
+
+                // Instantiate our graph object.
+                graph = new links.Graph3d(document.getElementById('mygraph'));
+
+                // Draw our graph with the created data and options
+                graph.draw(data, options);
+            },
+            dataType: 'json',
+            contentType: 'application/json'
+        });
+    }
+})();
